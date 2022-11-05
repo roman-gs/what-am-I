9313f5508fe61e9fc06a7411c7efce2d4ca12b64
roman-gs <roman_gs@hotmail.fr>
2022-11-05 18:20:56 +0000

    Change index.html layout and adjust functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,59 +2,69 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    displayRiddle();
+  //  displayRiddle();
 })
 
-//Different Riddles
-var teapot = {answer : "a teapot", question : "What begins with T ends with T and has T in it?", imgSource : "./assets/images/pexels-ekaterina-bolovtsova-5849991.jpg",};
-var sponge = {answer :"a sponge", question : "What is full of holes but still holds water?", imgSource : "./assets/images/pexels-cottonbro-4108680.jpg",};
-var candle = {answer:"a candle", question : "I'm tall when I'm young, and I'm short when I'm old, what am I?", imgSource : "./assets/images/pexels-pixabay-278823.jpg",};
-
-//Selecting random riddle (credit https://stackoverflow.com/questions/65129389/how-can-i-set-a-variable-to-one-of-two-random-options)
-var riddles = [teapot, sponge, candle];
-var index = Math.floor(Math.random() * riddles.length);
-var riddle = riddles[index];
-
-function displayRiddle() {
-    document.getElementById("riddle").textContent = riddle.question;
-}
-
-function checkAnswer() {
-
-    let userAnswer = document.getElementById("answer-box").value;
-    let isCorrect = userAnswer === riddle.answer;
-
-    if (isCorrect) {
-        alert("Congratulation, that's correct!");
-        incrementScore();
-    } else {
-        alert(`You've answered ${userAnswer}, the correct answer was ${riddle.answer}`);
-        resetScore();
-    }
-}
-
+//Increment score:
 function incrementScore() {
 
     let oldscore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldscore;
 }
 
-function resetScore() {
-    document.getElementById("score").innerText = "0"
-    nextQuestion();
+//Teapot riddle
+function checkAnswer1() {
+    let userAnswer = document.getElementById("answer-box1").value;
+    
+    if (userAnswer === "teapot") {
+        alert("Congratulation, that's correct!");
+        incrementScore();
+    } else {
+        alert(`You've answered ${userAnswer}, the correct answer was teapot`);
+    }
 }
+
+function changeImage1() {
+    document.getElementById("image").src = "./assets/images/pexels-ekaterina-bolovtsova-5849991.jpg";
+}
+
+//Sponge riddle
+function checkAnswer2() {
+    let userAnswer = document.getElementById("answer-box2").value;
+    
+    if (userAnswer === "sponge") {
+        alert("Congratulation, that's correct!");
+        incrementScore();
+    } else {
+        alert(`You've answered ${userAnswer}, the correct answer was sponge`);
+    }
+}
+
+function changeImage2() {
+    document.getElementById("image2").src = "./assets/images/pexels-cottonbro-4108680.jpg";
+}
+
+//Candle riddle
+function checkAnswer3() {
+    let userAnswer = document.getElementById("answer-box3").value;
+    
+    if (userAnswer === "candle") {
+        alert("Congratulation, that's correct!");
+        incrementScore();
+    } else {
+        alert(`You've answered ${userAnswer}, the correct answer was candle`);
+    }
+}
+
+function changeImage3() {
+    document.getElementById("image3").src = "./assets/images/pexels-pixabay-278823.jpg";
+}
+
 
 // Credit: https://stackoverflow.com/questions/6764961/change-an-image-with-onclick
-function changeImage() {
-        document.getElementById("image").src = riddle.imgSource;
-        resetScore();
-}
 
-function nextQuestion() {
-    document.getElementById("riddle").textContent = "";
-    document.getElementById("answer-box").value = "";
-    document.getElementById("riddle").textContent = riddle.question++;
-}
+
+
   
 
     
