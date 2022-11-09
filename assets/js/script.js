c0dce7bc9b587de272e010d884a3c333ce1331e8
roman-gs <roman_gs@hotmail.fr>
2022-11-09 17:30:07 +0000

    Add showMe functions for all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,71 +4,132 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 //Teapot riddle
-function checkAnswer1() {
-    let userAnswer = document.getElementById("answer-box1").value;
+function checkAnswerTeapot() {
+    let userAnswer = document.getElementById("answer-box-teapot").value;
     
     if (userAnswer === "teapot") {
         alert("Congratulation, that's correct!");
-        changeImage1();
+        changeImageTeapot();
         document.getElementById("image").style.border = "6px solid green";
+        document.getElementById("next").style.color = "green";
+        document.getElementById("next").style.fontWeight = "bold";
+        document.getElementById("next").style.fontSize = "150%";
     } else {
         alert(`You've answered ${userAnswer}, the correct answer was teapot`);
-        changeImage1();
+        changeImageTeapot();
         document.getElementById("image").style.border = "6px solid red";
+        document.getElementById("next").style.color = "red";
+        document.getElementById("next").style.fontWeight = "bold";
+        document.getElementById("next").style.fontSize = "150%";
     }
 }
 
-function changeImage1() {
+function changeImageTeapot() {
     document.getElementById("image").src = "./assets/images/pexels-ekaterina-bolovtsova-5849991.jpg";
 }
 
-//Sponge riddle
-function checkAnswer2() {
-    let userAnswer = document.getElementById("answer-box2").value;
-    
-    if (userAnswer === "sponge") {
-        alert("Congratulation, that's correct!");
-        changeImage2();
-        document.getElementById("image2").style.border = "6px solid green";
-    } else {
-        alert(`You've answered ${userAnswer}, the correct answer was sponge`);
-        changeImage2();
-        document.getElementById("image2").style.border = "6px solid red";
-    }
+function showMeTeapot() {
+    changeImageTeapot();
+    document.getElementById("image").style.border = "6px solid red";
+    document.getElementById("next").style.color = "red";
+    document.getElementById("next").style.fontWeight = "bold";
+    document.getElementById("next").style.fontSize = "150%";
 }
 
-function changeImage2() {
-    document.getElementById("image2").src = "./assets/images/pexels-cottonbro-4108680.jpg";
-}
-
-//Candle riddle
-function checkAnswer3() {
-    let userAnswer = document.getElementById("answer-box3").value;
-    
-    if (userAnswer === "candle") {
-        alert("Congratulation, that's correct!");
-        changeImage3();
-        document.getElementById("image3").style.border = "6px solid green";
-    } else {
-        alert(`You've answered ${userAnswer}, the correct answer was candle`);
-        changeImage3();
-        document.getElementById("image3").style.border = "6px solid red";
-    }
-}
-
-function changeImage3() {
-    document.getElementById("image3").src = "./assets/images/pexels-pixabay-278823.jpg";
-}
-
-/* Deactivate button  credit: https://stackoverflow.com/questions/7067005/disable-button-whenever-a-text-field-is-empty-dynamically */
-function btnActivation() { 
-    if(!document.getElementById('answer-box1').value.length){
-        document.getElementById("submit-btn1").disabled = true;            
+function btnActivationTeapot() { 
+    if(!document.getElementById("answer-box-teapot").value.length){
+        document.getElementById("submit-btn-teapot").disabled = true;            
     }else{
-        document.getElementById("submit-btn1").disabled = false;
+        document.getElementById("submit-btn-teapot").disabled = false;
 
     }           
 } 
+
+//Sponge riddle
+function checkAnswerSponge() {
+    let userAnswer = document.getElementById("answer-box-sponge").value;
+    
+    if (userAnswer === "sponge") {
+        alert("Congratulation, that's correct!");
+        changeImageSponge();
+        document.getElementById("image").style.border = "6px solid green";
+        document.getElementById("next").style.color = "green";
+        document.getElementById("next").style.fontWeight = "bold";
+        document.getElementById("next").style.fontSize = "150%";
+    } else {
+        alert(`You've answered ${userAnswer}, the correct answer was sponge`);
+        changeImageSponge();
+        document.getElementById("image").style.border = "6px solid red";
+        document.getElementById("next").style.color = "red";
+        document.getElementById("next").style.fontWeight = "bold";
+        document.getElementById("next").style.fontSize = "150%";
+    }
+}
+
+function changeImageSponge() {
+    document.getElementById("image").src = "./assets/images/pexels-ekaterina-bolovtsova-5849991.jpg";
+}
+
+function showMeSponge() {
+    changeImageSponge();
+    document.getElementById("image").style.border = "6px solid red";
+    document.getElementById("next").style.color = "red";
+    document.getElementById("next").style.fontWeight = "bold";
+    document.getElementById("next").style.fontSize = "150%";
+}
+
+function btnActivationSponge() { 
+    if(!document.getElementById("answer-box-sponge").value.length){
+        document.getElementById("submit-btn-sponge").disabled = true;            
+    }else{
+        document.getElementById("submit-btn-sponge").disabled = false;
+
+    }           
+} 
+//Candle riddle
+function checkAnswerCandle() {
+    let userAnswer = document.getElementById("answer-box-candle").value;
+    
+    if (userAnswer === "candle") {
+        alert("Congratulation, that's correct!");
+        changeImageCandle();
+        document.getElementById("image").style.border = "6px solid green";
+        document.getElementById("next").style.color = "green";
+        document.getElementById("next").style.fontWeight = "bold";
+        document.getElementById("next").style.fontSize = "150%";
+    } else {
+        alert(`You've answered ${userAnswer}, the correct answer was candle`);
+        changeImageCandle();
+        document.getElementById("image").style.border = "6px solid red";
+        document.getElementById("next").style.color = "red";
+        document.getElementById("next").style.fontWeight = "bold";
+        document.getElementById("next").style.fontSize = "150%";
+    }
+}
+
+function changeImageCandle() {
+    document.getElementById("image").src = "./assets/images/pexels-pixabay-278823.jpg";
+}
+
+function showMeCandle() {
+    changeImageCandle();
+    document.getElementById("image").style.border = "6px solid red";
+    document.getElementById("next").style.color = "red";
+    document.getElementById("next").style.fontWeight = "bold";
+    document.getElementById("next").style.fontSize = "150%";
+}
+
+function btnActivationCandle() { 
+    if(!document.getElementById("answer-box-candle").value.length){
+        document.getElementById("submit-btn-candle").disabled = true;            
+    }else{
+        document.getElementById("submit-btn-candle").disabled = false;
+
+    }           
+} 
+
+/* Deactivate button  credit: https://stackoverflow.com/questions/7067005/disable-button-whenever-a-text-field-is-empty-dynamically */
+
 
 
 
